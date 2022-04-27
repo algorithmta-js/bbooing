5c0b1e52f77a84d2c559d471c521eaac6e117ef1
jynam <njy1007@gmail.com>
2022-04-27 22:08:01 +0900

    2주차 과제

---
+++ 문제 풀이/백준_1931번_회의실_배정.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(file).toString().trim().split('\n');
+
+const [n, ...arr] = input;
+let answer = 0;
+const schedule = arr.map((num) => num.split(' ') // [시작 시간, 끝나는 시간] 형태의 배열로 회의 시간 표현
+                      .map((num)=> +num))
+                    .sort((a,b) => a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]); // 시작 시간 비교도 중요함
+
+let finish = -1;
+schedule.forEach((time) => {
+  if(time[0] >= finish) { // 끝나는 동시에 시작할 수 있기 때문에 같은 경우 포함
+    answer++;
+    finish = time[1];
+  }
+});
+
+console.log(answer);
