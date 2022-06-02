1c2ba4283e0c7480d36033da6be2226741503a95
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-06-02 16:01:35 +0900

    [feat] add SearchInput

---
+++ 구현 과제 풀이/프로그래밍 언어 검색/주함/components/SearchInput.js
@@ -0,0 +1,15 @@
+export default function SearchInput({ $target, initialState }) {
+  this.$element = document.createElement("form");
+  this.$element.className = "SearchInput";
+  this.state = initialState;
+
+  $target.appendChild(this.$element);
+
+  this.render = () => {
+    this.$element.innerHTML = `
+      <input class="SearchInput__input" type="text" placeholder="프로그램 언어를 입력하세욥." value="${this.state}">
+    `;
+  };
+
+  this.render();
+}
